1cb3be56e26d86a42299afef2d9e12e37c8e2b52
Your Name <you@example.com>
2024-09-02 22:17:32 +0300

    Add config and ASCII indicator by -c option

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from gi.repository import GLib, Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
 
+from config import refresh_time
+
 APPINDICATOR_ID = 'myappindicator'
 CURRPATH = os.path.dirname(__file__)
 
-refresh_time = 500
-
 parser = argparse.ArgumentParser(description='Indicate CPU load in panel.')
 parser.add_argument('-d', '--daemon', action='store_true')
+parser.add_argument('-c', '--clear', action='store_true')
 
 args = parser.parse_args()
 
@@ -61,40 +62,54 @@ class Indicator():
 
     def check_cpu_load(self):
         cpu_load = psutil.cpu_percent()
-        if not args.daemon:
-            print(cpu_load)
+        res = cpu_load
         if cpu_load < 10:
             self.indicator.set_icon_full(CURRPATH+"/10_prcnt.svg", 'CPU: 10%')
+            res = str(cpu_load) + ' =>---------|'
         if 20 > cpu_load > 10:
             self.indicator.set_icon_full(CURRPATH+"/20_prcnt.svg", 'CPU: 20%')
+            res = str(cpu_load) + ' ==>--------|'
         if 30 > cpu_load > 20:
             self.indicator.set_icon_full(CURRPATH+"/30_prcnt.svg", 'CPU: 30%')
+            res = str(cpu_load) + ' ===>-------|'
         if 40 > cpu_load > 30:
             self.indicator.set_icon_full(CURRPATH+"/40_prcnt.svg", 'CPU: 40%')
+            res = str(cpu_load) + ' ====>------|'
         if 50 > cpu_load > 40:
             self.indicator.set_icon_full(CURRPATH+"/50_prcnt_green.svg", 'CPU: 50%')
+            res = str(cpu_load) + ' =====>-----|'
         if 60 > cpu_load > 50:
             self.indicator.set_icon_full(CURRPATH+"/60_prcnt_yellow.svg", 'CPU: 60%')
+            res = str(cpu_load) + ' ======>----|'
         if 70 > cpu_load > 60:
             self.indicator.set_icon_full(CURRPATH+"/70_prcnt_yellow.svg", 'CPU: 70%')
+            res = str(cpu_load) + ' =======>---|'
         if 80 > cpu_load > 70:
             self.indicator.set_icon_full(CURRPATH+"/80_prcnt_orange.svg", 'CPU: 80%')
+            res = str(cpu_load) + ' ========>--|'
         if 90 > cpu_load > 80:
             self.indicator.set_icon_full(CURRPATH+"/90_prcnt_orange.svg", 'CPU: 90%')
+            res = str(cpu_load) + ' =========>-|'
         if 100 > cpu_load > 90:
             self.indicator.set_icon_full(CURRPATH+"/100_prcnt_red.svg", 'CPU: 100%')
+            res = str(cpu_load) + ' ==========>|'
         menu = gtk.Menu()
         menu.show_all()
+        return res
 
     def quit(self, source):
         gtk.main_quit()
 
     def on_timeout(self, data):
+        cpl = self.check_cpu_load()
         if not args.daemon:
-            print('InnerLoop')
-        self.check_cpu_load()
+            if args.clear:
+                clear()
+            print(f'CPU Usage: {cpl}')
         self.timeout_id = GLib.timeout_add(refresh_time, self.on_timeout, None)
 
+
+clear = lambda: os.system('clear')
 
 def main():
     #indicator = appindicator.Indicator.new(APPINDICATOR_ID, 'whatever', appindicator.IndicatorCategory.SYSTEM_SERVICES)
@@ -102,9 +117,10 @@ def main():
     #indicator.set_menu(gtk.Menu())
     ind = Indicator()
     gtk.main()
-    ind.main_loop()
+    #ind.main_loop()
     if not args.daemon:
         print('gtk main ended')
 
 if __name__ == "__main__":
     main()
+
